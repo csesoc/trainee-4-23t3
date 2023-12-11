38ef88c92d19077a94cab5ab3e81833a1a929e2c
Kim <kim.lam19442@gmail.com>
2023-12-11 11:14:04 +1100

    oops forgor to commit

--- src/components/youtube/Youtube.tsx
+++ src/components/youtube/Youtube.tsx
@@ -1,11 +1,38 @@
-import '../../App.css'
-// TEMP IMPORT REMOVE LATER
+import React, {useState} from 'react';
+import YouTube, {YouTubeProps} from 'react-youtube';
+import './Youtube.css';
 
 const Youtube = () => {
+  const onPlayerReady: YouTubeProps['onReady'] = (event) => {
+    event.target.pauseVideo();
+  }
+  
+  const opts: YouTubeProps['opts'] = {
+    height: '100%',
+    width: '40%',
+    playerVars: {
+      autoplay: 1,
+    }
+  }
+  const [value, setValue] = useState("");
+  const [updated, setUpdated] = useState(value);
+
+  const handleChange = (event : any) => {
+    setValue(event.target.value);
+  };
+  const handleClick = () => {
+    setUpdated(value);
+  };
   return (
-    <>
-      <div id='temp-box'/>
-    </>
+  <>
+  <div id="youtube">
+    <div id="input-align">
+      <input type="text" id="value" name="value" onChange={handleChange} value={value} />
+      <button id="send-link" onClick={handleClick}>load</button>
+    </div>
+    <YouTube videoId={updated} opts={opts} onReady={onPlayerReady} />
+  </div>
+  </>
   )
 }
 
