24e179b184e9135ef859945b214e08f98072133d
Anish Sinha <anishsinha@Anishs-MBP.modem>
2023-12-12 16:05:28 +1100

    more scaling issues fixed

--- src/components/github/Github.tsx
+++ src/components/github/Github.tsx
@@ -15,6 +15,9 @@ const Github = () => {
   }
 
   let githubHash = new Map<String, Number>();
+  let monthsXAxis: string[] = ["Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"];
+
+
   let idx = 1;
   if (isLeapYear()) {
     while (idx <= 366) {
@@ -53,54 +56,47 @@ const Github = () => {
   return (
     <>
       <div className='github-total-container'>
-        <p className="xaxis">
-          <span className="nowrap">Jan</span>
-          <span className="nowrap">Feb</span>
-          <span className="nowrap">March</span>
-          <span className="nowrap">April</span>
-          <span className="nowrap">May</span>
-          <span className="nowrap">June</span>
-          <span className="nowrap">July</span>
-          <span className="nowrap">Aug</span>
-          <span className="nowrap">Sept</span>
-          <span className="nowrap">Oct</span>
-          <span className="nowrap">Nov</span>
-          <span className="nowrap">Dec</span>
-        </p>
-        <div className='github-container'>
-          {Array.from(githubHash.entries()).map(([date, productiviyScore]) => (
+        <div className='scroll'>
+          <p className="xaxis">
+            {monthsXAxis.map((month) => (
+              <span className="nowrap">{month}</span>
+            ))}
+          </p>
+          <div className='github-container'>
+            {Array.from(githubHash.entries()).map(([date, productiviyScore]) => (
             productiviyScore === 0 ? (
-            <>
-              <div className="column-4 github-scale-0" data-tooltip={`No productivity on ${date}`}>
-              </div>
-            </>
-          ) : productiviyScore === 1 ? (
-            <>
-              <div className="column-4 github-scale-1" data-tooltip={`Minimal productivity on ${date}`}>
-              </div>
-            </>
-          ) : productiviyScore === 2 ? (
-            <>
-              <div className="column-4 github-scale-2" data-tooltip={`Moderate productivity on ${date}`}>
-              </div>
-            </>
-          ) : productiviyScore === 3 ? (
-            <>
-              <div className="column-4 github-scale-3" data-tooltip={`Significant productivity on ${date}`}>
-              </div>
-            </>
-          ) : (
-            <>
-              <div className="column-4 github-scale-4" data-tooltip={`Substantial productivity on ${date}`}>
-              </div>
-            </>
-          )
-          ))}
-        </div>
-        <div className='position-github-scale'>
-          <span className='span-github-scale-style-less'>Less</span>&nbsp;&nbsp;
-          <img className='github-scale-style' src={scale} alt="Description of the image" />
-          &nbsp;&nbsp;<span className='span-github-scale-style-more'>More</span>
+              <>
+                <div className="column-4 github-scale-0" data-tooltip={`No productivity on ${date}`}>
+                </div>
+              </>
+            ) : productiviyScore === 1 ? (
+              <>
+                <div className="column-4 github-scale-1" data-tooltip={`Minimal productivity on ${date}`}>
+                </div>
+              </>
+            ) : productiviyScore === 2 ? (
+              <>
+                <div className="column-4 github-scale-2" data-tooltip={`Moderate productivity on ${date}`}>
+                </div>
+              </>
+            ) : productiviyScore === 3 ? (
+              <>
+                <div className="column-4 github-scale-3" data-tooltip={`Significant productivity on ${date}`}>
+                </div>
+              </>
+            ) : (
+              <>
+                <div className="column-4 github-scale-4" data-tooltip={`Substantial productivity on ${date}`}>
+                </div>
+              </>
+            )
+            ))}
+          </div>
+          <div className='position-github-scale'>
+            <span className='span-github-scale-style-less'>Less</span>&nbsp;&nbsp;
+            <img className='github-scale-style' src={scale} alt="Description of the image" />
+            &nbsp;&nbsp;<span className='span-github-scale-style-more'>More</span>
+          </div>
         </div>
       </div>
     </>
