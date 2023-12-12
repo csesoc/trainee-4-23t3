680548c2885eaf9100de8d63a4f6e80c1d7235a6
CHUN Nicole Wai Yan <19chunnw3@Nicole-Chun-MacBookPro-2.local>
2023-12-12 11:34:15 +1100

    finished timer

--- src/components/timer/clock/Clock.tsx
+++ src/components/timer/clock/Clock.tsx
@@ -1,6 +1,9 @@
 import { useContext, useEffect, useState, useRef } from "react";
 import TimerContext from "../TimerContext";
 import './Clock.css';
+import PlayButton from "./PlayButton";
+import PauseButton from "./PauseButton";
+import ResetButton from "./ResetButton";
 
 const Clock = () => {
     const timerInfo = useContext(TimerContext);
@@ -55,12 +58,17 @@ const Clock = () => {
         <div id="clock-container">
             <div id="clock-left">
                 {isStudying ? <h2 id='status-text'>back to work...</h2> : <h2 id='status-text'>time for a break!</h2>}
-                <h1 id='digit-text'>{`${mins} : ${clockDigit(secs)}`}</h1>
+                <h1 id='digit-text'>{`${mins}:${clockDigit(secs)}`}</h1>
             </div>
 
             <div id="clock-right">
-                <button id='clock-button' onClick={() => {isPausedRef.current = !isPaused; setisPaused(isPausedRef.current)}}>{isPaused ? "Start" : "Pause"}</button>
-                <button id='clock-button' onClick={() => reset()}>Reset</button>
+                <div>
+                    <button id='icon-container' 
+                        onClick={() => {isPausedRef.current = !isPaused; setisPaused(isPausedRef.current)}}>
+                        {isPaused ? <PlayButton/> : <PauseButton/>}
+                    </button>
+                    <button id='icon-container' onClick={() => reset()}><ResetButton/></button>
+                </div>
                 <button id='clock-button' onClick={() => timerInfo.setShowSettings(true)}>Settings</button>
             </div>
         </div>
